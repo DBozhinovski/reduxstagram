e886b02dce27685baeb0419abd3fdb6cfba65537
Darko Bozhinovski <dbozhinovski@gmail.com>
2016-10-18 01:51:23 +0200

    Initial; Lesson 10

---
+++ client/actions/actionCreator.js
@@ -0,0 +1,32 @@
+// increment 
+function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  };
+}
+
+// add comment
+function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+// remove comment
+function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
+
+export default {
+  increment,
+  addComment,
+  removeComment
+}
